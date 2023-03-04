dd16c30b834f9048d963ba4520579ea32fa902a1
rony02120 <habibur02@gmail.com>
2023-03-04 13:53:17 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,151 @@
+/*Habibur Rahman
+
+package main
+import ("fmt")
+func main () {
+// String "" ''='my name "MD. Habibur rahman" my hometown'
+// integar 0-9= int ( ata songkhape likha hoi)
+// Float 9.4 = float
+fmt.Println(5+5)
+fmt.Println(" Md. Habibur Rahman")
+fmt.Println(5.5+3.5)
+}
+*/
+
+// int
+// float32
+// String
+// Bool
+
+/*package main
+import ("fmt")
+func main () {
+// var= variable = value
+var name string= "10"
+x:="Md. Habibur rahman"
+fmt.Println(name)
+fmt.Println(x)
+}
+*/
+
+/*package main
+import ("fmt")
+func main () {
+var a string
+var b int
+var c bool
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println(c)
+}
+*/
+
+
+package main
+import ("fmt")
+func main () {
+var a,b,c,d int=10,11,15,20
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println(c)
+fmt.Println(d)
+fmt.Println(a,b,c,d) //Maltbel Variable
+}
+
+// Maltbel Variable name rules
+
+// habibur=
+// a=
+// habibur_Rahman=
+// habibur123=
+// _habiburrahman123=
+// 123habibur= not Right
+// string likte gele () use Korte Hobe
+// constant Pai= 3.14 g=9.8 G=11.-11
+
+/*package main
+import ("fmt")
+const A int=1
+func main () {
+a=2
+fmt.Println(A)
+}
+*/
+
+
+/*package main
+import ("fmt")
+const (
+A int=1
+B =3.1
+C ="Habibur"
+)
+func main () {
+fmt.Println(A)
+fmt.Println(B)
+fmt.Println(C)
+}
+*/
+
+// output Fountion
+// Print()
+// Println()
+// printf()
+// 
+
+/*package main
+import ("fmt")
+func main () {
+var i,j string=
+"habibur","sumon"
+fmt.Print(i,"\n")
+fmt.Print(j,"\n")// \n enter use 
+fmt.Print(i,"\n",j,"\n")
+fmt.Print(i," ",j)
+}
+*/
+// tomar name ki=amar name habibur
+
+/*package main
+import ("fmt")
+func main () {
+var name string="habibur"
+var age int=28
+//fmt.Printf("name is valu: %v and type: %T\n", name,name)
+//fmt.Printf("age is valu: %v and type: %T\n", age,age)
+fmt.Printf("name is valu: %v\n", name)
+fmt.Printf("age is valu: %v ", age)
+}
+*/
+
+// Printf  
+// %v print the value in the default format
+// %#v print the value go sentesk format
+// %T print the value of type
+// %% print % sign
+
+/*package main
+import ("fmt")
+func main () {
+var i=3.5
+var Txt="habibur"
+fmt.Printf("%v\n",i)
+fmt.Printf("%v#\n",i)
+fmt.Printf("%v%%\n",i)
+fmt.Printf("%T\n",i)
+fmt.Printf("%v\n",Txt)
+fmt.Printf("%v#\n",Txt)
+fmt.Printf("%v%%\n",Txt)
+fmt.Printf("%T\n",Txt)
+}
+*/
+
+
+
+
+
+
+
+
+
+
